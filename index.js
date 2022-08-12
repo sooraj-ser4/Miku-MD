dcda0e85f405af6138319c505e5d2e3ba9ed81d4
sooraj-ser4 <110798829+sooraj-ser4@users.noreply.github.com>
2022-08-12 06:51:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ console.log(color(figlet.textSync('Miku Bot MD', {
 		whitespaceBreak: true
         }), 'yellow'))
 
-console.log(color('\nHello, I am Fantox, the main developer of this bot.\n\nThanks for using: Miku Bot','aqua'))
+console.log(color('\nHello, I am Christy, the main developer of this bot.\n\nThanks for using: Miku Bot','aqua'))
 console.log(color('\nYou can follow me on GitHub: FantoX001','aqua'))
 
     let { version, isLatest } = await fetchLatestBaileysVersion()
     const Miku = MikuConnect({
         logger: pino({ level: 'silent' }),
         printQRInTerminal: true,
-        browser: ['Miku by: Fantox','Safari','1.0.0'],
+        browser: ['Miku by: Christy','Safari','1.0.0'],
         auth: state,
         version
     })
